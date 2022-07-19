84843a176a43f20d094dbd746db844fc12136e94
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-19 14:30:44 +0300

    generate random meme functionality

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,7 +1,22 @@
 import React from "react";
 import '../styles.css';
+import memesData from "../memesData";
 
 function Meme(){
+    const memeArray = memesData.data.memes;
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getRandomMeme(){
+        const randomIndex = Math.floor(Math.random() * memeArray.length);
+        const image_url = memeArray[randomIndex].url;
+        console.log(image_url);
+        setMemeImage(image_url);
+    }
+
+    function handleClick(){
+        getRandomMeme();
+    }
+    
     return(
         <section>
             <div className="form">
@@ -9,9 +24,12 @@ function Meme(){
                     <input className="form-input" type='text' placeholder='Shut up'></input>
                     <input className="form-input" type='text' placeholder='and take my money'></input>
                 </div>
-                <button className="form--button">
-                Get a new meme image  🖼 
+                <button onClick={handleClick} className="form--button">
+                    Get a new meme image  🖼 
                 </button>
+            </div>
+            <div className="form--image">
+                <img src={memeImage} className="meme--image"/>
             </div>
         </section>
     )
