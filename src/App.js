16f2acbe15565779e63b74a00d2e38564b5d29ff
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-20 21:21:54 +0300

    adds practice component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Header from "./components/Header";
 import Meme from "./components/Meme";
+import Practice from "./components/practice/Practice";
 
 function App() {
   return (
     <>
-      <Header />
-      <Meme />
+      <Practice />
     </>
   );
 }
