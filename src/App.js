31937c779c97928ff0219e32500b3bbe78e3d113
Faith Mutitu <mutitufaith53@gmail.com>
2022-07-25 12:12:24 +0300

    renders meme component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import Boxes from "./components/boxes/TestBox";
+import Meme from './components/Meme';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
-      <Boxes />
+      <Header />
+      <Meme />
     </>
   );
 }
